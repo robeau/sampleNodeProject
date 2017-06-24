66b6312e64822df65d7ce2aa978ceeb75495539e
Natalie Romano <brandnew03@gmail.com>
2017-06-24 16:04:36 -0700

    end day 1

--- public/script.js
+++ public/script.js
@@ -1,6 +1,28 @@
 'use strict';
 
-var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest(),
+    submit = document.querySelectorAll('#submit')[0];
+
+xhr.open("GET", "/read");
+xhr.send();
+
+submit.addEventListener('click', function() {
+  var name = document.querySelectorAll('#name')[0],
+      age = document.querySelectorAll('#age')[0],
+      addXhr = new XMLHttpRequest(),
+      data = 'age=' + age.value + '&name=' + name.value;
+
+  addXhr.open("POST", "/add", true);
+  addXhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  addXhr.send(data);
+  addXhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      xhr.open("GET", "/read");
+      xhr.send();
+      showPeople();
+  }
+});
+});
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
@@ -8,15 +30,13 @@ xhr.addEventListener("readystatechange", function () {
   }
 });
 
-xhr.open("GET", "/read");
-xhr.send();
-
 function showPeople(data) {
   var ul = document.querySelectorAll('#people')[0],
       peopleArr = [];
 
   for (var key in data) peopleArr.push(data[key]);
 
+  ul.innerHTML = '';
   peopleArr.forEach(function(person) {
     var li = document.createElement('li');
     li.append(person.name);
