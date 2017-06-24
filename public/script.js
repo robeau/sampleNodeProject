4947f66e6011becbc20bac11db779c66d9e33ad6
Natalie Romano <brandnew03@gmail.com>
2017-06-24 14:45:00 -0700

    added get from front-end

--- public/script.js
+++ public/script.js
@@ -1,1 +1,25 @@
-console.log('hello from client');
+'use strict';
+
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    showPeople(JSON.parse(this.responseText));
+  }
+});
+
+xhr.open("GET", "/read");
+xhr.send();
+
+function showPeople(data) {
+  var ul = document.querySelectorAll('#people')[0],
+      peopleArr = [];
+
+  for (var key in data) peopleArr.push(data[key]);
+
+  peopleArr.forEach(function(person) {
+    var li = document.createElement('li');
+    li.append(person.name);
+    ul.append(li);
+  });
+}
